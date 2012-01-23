bd755afede28ff486300af950af55384019ae01a
Thomas Baum <thomas.baum@atns.de>
2012-01-23 17:18:22 +0100

    update neo4js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ require('coffee-script');
 var url = process.env.NEO4J_URL || 'http://localhost:7474';
 var neo4js = require('neo4js');
 
-var graphDb = new neo4js.GraphDatabase(url);
+var graphDb = new neo4js.GraphDatabase4Node(url);
 
 var App = require('./myapp');
 
